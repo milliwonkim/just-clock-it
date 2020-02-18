ef5cf2a2d8be5057c191e74d8ccf6ffb1dcd7504
milliwonkim <rldnjs9347@gmail.com>
2020-02-19 00:59:03 +0900

    Updates

---
+++ precache-manifest.cf3cc7cf06b2bd07d001e2eea04fe0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9d45f1d74c06585a83f85e71ac37b9",
+    "url": "/just-clock-it/index.html"
+  },
+  {
+    "revision": "323ba6a22e4f61362fde",
+    "url": "/just-clock-it/static/css/main.d8f80f22.chunk.css"
+  },
+  {
+    "revision": "7f72520bda6e649fd997",
+    "url": "/just-clock-it/static/js/2.31d7c685.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/just-clock-it/static/js/2.31d7c685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323ba6a22e4f61362fde",
+    "url": "/just-clock-it/static/js/main.e5474be8.chunk.js"
+  },
+  {
+    "revision": "0472de089c5eb021ed01",
+    "url": "/just-clock-it/static/js/runtime-main.10cf9ada.js"
+  }
+]);
